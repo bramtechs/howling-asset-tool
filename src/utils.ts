4185c35788b5224525df14a15f8d0a10efb82842
bramtechs <bramtech@telenet.be>
2024-11-23 01:17:17 +0100

    draw sides spinboxes

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { QLabel } from "@nodegui/nodegui";
+
 export function getWorkingDirectory() {
     return process.cwd();
 }
@@ -7,4 +9,10 @@ export function removePrefix(text: string, prefix: string): string {
         return text.substring(prefix.length);
     }
     return text;
+}
+
+export function quickLabel(text: string): QLabel {
+    const label = new QLabel();
+    label.setText(text);
+    return label;
 }
--- src/widgets/tabs/individual/nine-patch/NinePatchTabWidget.ts
+++ src/widgets/tabs/individual/nine-patch/NinePatchTabWidget.ts
@@ -1,5 +1,13 @@
-import { Direction, QBoxLayout, QGroupBox, QTabWidget } from "@nodegui/nodegui";
+import {
+    Direction,
+    QBoxLayout,
+    QGridLayout,
+    QGroupBox,
+    QSpinBox,
+    QTabWidget,
+} from "@nodegui/nodegui";
 import { ActiveAssetController } from "controllers/ActiveAssetController";
+import { quickLabel } from "utils";
 
 export class NinePatchTabWidget extends QTabWidget {
     private readonly controller: ActiveAssetController;
@@ -12,9 +20,23 @@ export class NinePatchTabWidget extends QTabWidget {
         this.setLayout(layout);
 
         // TODO: draw the nine-patch grid preview
-
         const groupBox = new QGroupBox();
-        groupBox.setTitle("Cells");
+        {
+            function side(label: string, row: number) {
+                groupLayout.addWidget(quickLabel(label), row, 0);
+                const leftDial = new QSpinBox();
+                groupLayout.addWidget(leftDial, row, 1);
+            }
+
+            groupBox.setTitle("Sides");
+            const groupLayout = new QGridLayout();
+            groupBox.setLayout(groupLayout);
+
+            side("Left", 0);
+            side("Right", 1);
+            side("Top", 2);
+            side("Bottom", 3);
+        }
 
         layout.addWidget(groupBox);
     }
