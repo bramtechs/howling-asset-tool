692357f6ce7ae32661b85231b16fac989404084f
bramtechs <bramtech@telenet.be>
2024-11-23 22:32:38 +0100

    project group box

--- src/controllers/RootController.ts
+++ src/controllers/RootController.ts
@@ -3,7 +3,17 @@ import { ActiveAssetController } from "./ActiveAssetController";
 export class RootController {
     activeAssetController: ActiveAssetController;
 
+    private _outputDirectory: string | undefined;
+
     constructor() {
         this.activeAssetController = new ActiveAssetController();
+    }
+
+    get outputDirectory(): string | undefined {
+        return this._outputDirectory;
+    }
+
+    setOutputDirectory(directory: string): void {
+        this._outputDirectory = directory;
     }
 }
